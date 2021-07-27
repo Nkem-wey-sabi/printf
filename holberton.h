2f174afaa42c056da6d3e298b59a33977716b8e4
Oluwaseun ALX <spreadwebng@gmail.com>
2021-07-27 06:55:55 +0100

    update

--- holberton.h
+++ holberton.h
@@ -1,28 +1,6 @@
 #ifndef HEADER_HOLBERTON
 #define HEADER_HOLBERTON
 #include <stdarg.h>
-#ifndef our_printf
-#define our_printf
-#include <stdio.h>
-/**
- * struct specifier - struct specifier
- * @valid: the valid character.
- * @f: the functions associated.
- *
- */
-typedef struct specifier
-{
-	char *valid;
-	int (*f)(va_list);
-} spec;
-int _printf(const char *format, ...);
-int print_c(va_list args);
-int print_s(va_list args);
-int print_d(va_list args);
-int print_i(va_list args);
-int _putchar(char c);
-int print_percent(va_list args);
-int (*get_func(char x))(va_list args);
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
