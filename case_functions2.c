dba8073356b38538f6bc645b61becc378cb4eebb
Nkem-wey-sabi <kemcho.ogor@gmail.com>
2021-07-27 04:25:06 -0700

    Another correction

--- case_functions2.c
+++ case_functions2.c
@@ -1,22 +1,12 @@
 #include "holberton.h"
 #include <stdarg.h>
 
-/**
- * print_ptr - print_base16_upper_lower
- * @arg: va_list parameter
- * Description: This function print address pointer
- * in representation parameter for print hexadecimal format
- * Return: address pointer
- */
-
-unsigned int convert_x(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_X(va_list args, buffer_t *output,
+unsigned int convert_S(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 
 /**
- * convert_x - Converts an unsigned int argument to hex using abcdef
- *             and stores it to a buffer contained in a struct.
+ * convert_S - Converts an argument to a string and
+ *             stores it to a buffer contained in a struct.
  * @args: A va_list pointing to the argument to be converted.
  * @flags: Flag modifiers.
  * @wid: A width modifier.
@@ -25,71 +15,46 @@ unsigned int convert_X(va_list args, buffer_t *output,
  * @output: A buffer_t struct containing a character array.
  *
  * Return: The number of bytes stored to the buffer.
- */
-unsigned int convert_x(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len)
-{
-	unsigned long int num;
-	unsigned int ret = 0;
-	char *lead = "0x";
-
-	if (len == LONG)
-		num = va_arg(args, unsigned long int);
-	else
-		num = va_arg(args, unsigned int);
-	if (len == SHORT)
-		num = (unsigned short)num;
-
-	if (HASH_FLAG == 1 && num != 0)
-		ret += _memcpy(output, lead, 2);
-
-	if (!(num == 0 && prec == 0))
-		ret += convert_ubase(output, num, "0123456789abcdef",
-				flags, wid, prec);
-
-	ret += print_neg_width(output, ret, flags, wid);
-
-	return (ret);
-}
-
-/**
- * convert_X - Converts an unsigned int argument to hex using ABCDEF
- *             and stores it to a buffer contained in a struct.
- * @args: A va_list pointing to the argument to be converted.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
- * @len: A length modifier.
- * @output: A buffer_t struct containing a character array.
  *
- * Return: The number of bytes stored to the buffer.
+ * Description: Non-printable characteres (ASCII values < 32 or >= 127)
+ *              are stored as \x followed by the ASCII code value in hex.
  */
-unsigned int convert_X(va_list args, buffer_t *output,
+unsigned int convert_S(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {
-	unsigned long int num;
+	char *str, *null = "(null)", *hex = "\\x", zero = '0';
+	int size, index;
 	unsigned int ret = 0;
-	char *lead = "0X";
 
-	if (len == LONG)
-		num = va_arg(args, unsigned long);
-	else
-		num = va_arg(args, unsigned int);
-	if (len == SHORT)
-		num = (unsigned short)num;
+	(void)len;
+	str = va_arg(args, char *);
+	if (str == NULL)
+		return (_memcpy(output, null, 6));
 
-	if (HASH_FLAG == 1 && num != 0)
-		ret += _memcpy(output, lead, 2);
+	for (size = 0; str[size];)
+		size++;
 
-	if (!(num == 0 && prec == 0))
-		ret += convert_ubase(output, num, "0123456789ABCDEF",
-				flags, wid, prec);
+	ret += print_string_width(output, flags, wid, prec, size);
+
+	prec = (prec == -1) ? size : prec;
+	for (index = 0; *(str + index) != '\0' && index < prec; index++)
+	{
+		if (*(str + index) < 32 || *(str + index) >= 127)
+		{
+			ret += _memcpy(output, hex, 2);
+			if (*(str + index) < 16)
+				ret += _memcpy(output, &zero, 1);
+			ret += convert_ubase(output, *(str + index),
+					     "0123456789ABCDEF", flags, 0, 0);
+			continue;
+		}
+		ret += _memcpy(output, (str + index), 1);
+	}
 
 	ret += print_neg_width(output, ret, flags, wid);
 
 	return (ret);
 }
-
 
 /**
  * print_rot13 - prints a string using rot13
