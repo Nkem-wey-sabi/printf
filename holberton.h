8efa82401476db1385fa1cc5e6dfba8ba9379e26
Oluwaseun ALX <spreadwebng@gmail.com>
2021-07-27 06:54:00 +0100

    update

---
+++ holberton.h
@@ -0,0 +1,44 @@
+#ifndef HEADER_HOLBERTON
+#define HEADER_HOLBERTON
+#include <stdarg.h>
+#ifndef our_printf
+#define our_printf
+#include <stdio.h>
+/**
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
+ *
+ */
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+int _printf(const char *format, ...);
+char *convert_to(char representation[], unsigned int num, int base);
+int function_manager(char c, va_list arg);
+int _switch(char c, va_list arg);
+int print_character(va_list arg);
+int print_sign(va_list arg, int base);
+int print_unsign(va_list arg, int base);
+int print_string(va_list arg);
+int print_ptr(va_list arg);
+int print_rot13(va_list arg);
+int print_rev(va_list arg);
+int print_base16_upper_lower(va_list arg, char *representation);
+int loop_format(va_list arg, const char *string);
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux);
+#endif
